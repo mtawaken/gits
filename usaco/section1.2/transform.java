6cc2b6adb2be4dc2571886ddbb96e3b7f384b73c
mtawaken <mtawaken@gmail.com>
2013-06-07 10:16:53 +0800

    Update transform.java

--- usaco/section1.2/transform.java
+++ usaco/section1.2/transform.java
@@ -8,7 +8,7 @@ import java.io.*;
 
 public class transform {
 
-  public static void main(String args[]) throws IOException {
+	public static void main(String args[]) throws IOException {
 		Scanner input = new Scanner(new FileReader("transform.in"));
 		PrintWriter output = new PrintWriter(new BufferedWriter(new FileWriter(
 				"transform.out")));
@@ -17,45 +17,53 @@ public class transform {
 		char[][] orig = new char[N][];
 		char[][] trans = new char[N][];
 		char[][] tmp = new char[N][];
+		char[][] tmp_orig = new char[N][];
 		String tmpStr;
 		for (int i = 0; i < N; i++) {
 			tmpStr = input.nextLine();
 			orig[i] = tmpStr.toCharArray();
 			tmp[i] = new char[N];
+			tmp_orig[i] = new char[N];
 		}
 		for (int i = 0; i < N; i++) {
 			tmpStr = input.nextLine();
 			trans[i] = tmpStr.toCharArray();
 		}
 		int result = 7;
-		//try #6
-		if (equal(orig, trans, N)) {
-			result = 6;
-		}
-		//try #1~3
+
+		// try #1~3
 		if (result == 7) {
-			copy(orig, tmp, N);
+			copy(orig, tmp_orig, N);
 			for (int i = 1; i <= 3; i++) {
-				rotate90(tmp, tmp, N);
+				rotate90(tmp_orig, tmp, N);
 				if (equal(trans, tmp, N)) {
 					result = i;
 					break;
 				}
+				copy(tmp, tmp_orig, N);
 			}
 		}
-		//try #4~5
+		// try #4~5
 		if (result == 7) {
 			reflect(orig, tmp, N);
 			if (equal(trans, tmp, N)) {
 				result = 4;
 			} else {
+				copy(tmp, tmp_orig, N);
 				for (int i = 0; i < 3; i++) {
-					rotate90(tmp, tmp, N);
+					rotate90(tmp_orig, tmp, N);
 					if (equal(trans, tmp, N)) {
 						result = 5;
 						break;
 					}
+					copy(tmp, tmp_orig, N);
 				}
+			}
+		}
+		// try #6
+		if (result == 7) {
+			if (equal(orig, trans, N)) {
+				result = 6;
 			}
 		}
 		output.println(result);
@@ -73,30 +81,31 @@ public class transform {
 	}
 
 	public static void rotate90(char[][] orig, char[][] tmp, int N) {
-		for (int level = N; level >= 2; level--) {
-			for (int i = level - N; i < level - 1; i++) {
-				tmp[level - N][i] = orig[level - 1 - i][level - N];
-				tmp[i][level - 1] = orig[level - N][i];
-				tmp[level - 1][i] = orig[level - 1 - i][level - 1];
-				tmp[i][level - N] = orig[level - 1][i];
+		copy(orig, tmp, N);
+		for (int i = 0; i < N / 2; i++) {
+			for (int j = i; j < N - i; j++) {
+				tmp[i][j] = orig[N - j - 1][i];
+				tmp[j][N - i - 1] = orig[i][j];
+				tmp[N - i - 1][j] = orig[N - j - 1][N - i - 1];
+				tmp[j][i] = orig[N - i - 1][j];
 			}
 		}
-		print(tmp,N);
+		print(tmp, N);
 	}
 
 	public static void reflect(char[][] orig, char[][] tmp, int N) {
 		for (int i = 0; i < N / 2; i++) {
 			for (int j = 0; j < N; j++) {
-				tmp[i][j] = orig[N - i - 1][j];
-				tmp[N - i - 1][j] = orig[i][j];
+				tmp[j][i] = orig[j][N - i - 1];
+				tmp[j][N - i - 1] = orig[j][i];
 			}
 		}
 		if (N % 2 == 1) {
 			for (int i = 0; i < N; i++) {
-				tmp[N / 2 + 1][i] = orig[N / 2 + 1][i];
+				tmp[i][N / 2] = orig[i][N / 2];
 			}
 		}
-		print(tmp,N);
+		print(tmp, N);
 	}
 
 	public static boolean equal(char[][] orig, char[][] tmp, int N) {
@@ -108,12 +117,12 @@ public class transform {
 		}
 		return true;
 	}
-	
-	public static void print(char[][] tmp,int N){
+
+	public static void print(char[][] tmp, int N) {
 		System.out.println("changing to:");
 		for (int i = 0; i < N; i++) {
 			for (int j = 0; j < N; j++) {
-				System.out.print(tmp[i][j]+" ");
+				System.out.print(tmp[i][j]);
 			}
 			System.out.println();
 		}
